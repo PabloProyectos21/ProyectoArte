e03948ba58b575aa0a87a9d7c9e251612b7e68de
PabloProyectos21 <pabloruce@gmail.com>
2025-05-22 10:20:24 +0200

    cambio importante 3

--- database/seeders/CommercialSeeder.php
+++ database/seeders/CommercialSeeder.php
@@ -11,24 +11,23 @@ class CommercialSeeder extends Seeder
     {
         $images = ['ad2.png', 'ad3.png', 'ad4.png', 'ad5.png'];
 
-        // Asegúrate de que los archivos están en el storage correcto
-        foreach (['ad2.png', 'ad3.png', 'ad4.png', 'ad5.png'] as $img) {
-            $src = database_path('seeders/commercials/' . $img);
-            $dst = storage_path('app/public/commercials/' . $img);
+        foreach ($images as $image) {
+            $src = database_path('seeders/commercials/' . $image);
+            $dst = storage_path('app/public/commercials/' . $image);
             if (!file_exists(dirname($dst))) {
                 mkdir(dirname($dst), 0775, true);
             }
-            copy($src, $dst);
-            @chmod($dst, 0664);
-        }
+            if (!file_exists($dst)) {
+                copy($src, $dst);
+                @chmod($dst, 0664);
+            }
 
-        // Ahora crea los commercials como ya haces
-        foreach ($images as $image) {
             Commercial::factory()->create([
                 'image' => "commercials/{$image}",
             ]);
         }
     }
+
 }
 
 
