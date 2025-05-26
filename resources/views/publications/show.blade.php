724a9b30fb290173c38d5440c10bc5d080e2b653
PabloProyectos21 <pabloruce@gmail.com>
2025-05-26 10:12:11 +0200

    todo bien

--- resources/views/publications/show.blade.php
+++ resources/views/publications/show.blade.php
@@ -88,45 +88,22 @@
                     </svg>
                 </button>
                 @elseif (!Auth::check())
-
-                    <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"  class="p-1 rounded-full transition text-blue-400 hover:text-purple-500" type="button">
-
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 24 24"
-                             fill="currentColor"
-                             stroke="currentColor"
-                             stroke-width="1.5"
-                             class="size-6">
-                            <path stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                        </svg>
-                    </button>
-                    <div id="popup-modal" role="dialog" aria-modal="true" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                        <div class="relative p-4 w-full max-w-md max-h-full">
-                            <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-                                <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                    </svg>
-                                    <span class="sr-only">Close modal</span>
-                                </button>
-                                <div class="p-4 md:p-5 text-center">
-                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                                    </svg>
-                                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">You need to be logged in to follow other artists</h3>
-                                    <a href="{{ route('login') }}">
-                                        <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-400 hover:bg-pink-400 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                                            Go to login page
-                                        </button>
-                                    </a>
-                                    <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                    <form method="GET" action="{{ route('login') }}" class="p-0 w-full sm:w-auto">
+                        <button
+                            type="submit"
+                            class="p-1 rounded-full transition text-blue-400 hover:text-purple-500">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 24 24"
+                                 fill="currentColor"
+                                 stroke="currentColor"
+                                 stroke-width="1.5"
+                                 class="size-6">
+                                <path stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                            </svg>
+                        </button>
+                    </form>
                 @endif
 
                 <span id="likes-count" class="text-gray-700 dark:text-gray-300">
